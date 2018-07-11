4cc002265ec5dadc372c7444783076844e2814d8
nanamikamata <nanami4437@gmail.com>
2018-07-11 16:46:16 +0900

    0711javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+document.write("問1");
+document.write("<br>");
+for(var i=1;i<6;i++){
+    document.write("★");
+}
+
+document.write("<br><br>");
+
+document.write("問2");
+document.write("<br>");
+for(var i=1;i<3;i++){
+    document.write("★");
+}
+
